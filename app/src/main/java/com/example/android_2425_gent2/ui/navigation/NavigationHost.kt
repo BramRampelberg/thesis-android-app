fde9a47f14ec30491135499eb3ccb5140e6afb45
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-12-08 21:22:03 +0100

    fix: issue with profile page import in NavigationHost

--- app/src/main/java/com/example/android_2425_gent2/ui/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/android_2425_gent2/ui/navigation/NavigationHost.kt
@@ -7,7 +7,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.android_2425_gent2.ui.screens.CalendarPage
 import com.example.android_2425_gent2.ui.screens.HomePage
-import com.example.android_2425_gent2.ui.screens.ProfilePage
 
 @Composable
 fun NavigationHost(navController: NavHostController, modifier: Modifier) {
