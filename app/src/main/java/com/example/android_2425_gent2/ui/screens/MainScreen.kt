b5ab1a73ad936d04c575b310a63135e6e7f735ec
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-12-13 15:17:02 +0100

    feat: SideNavigationRail for bigger devices

--- app/src/main/java/com/example/android_2425_gent2/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/android_2425_gent2/ui/screens/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.example.android_2425_gent2.ui.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
@@ -7,36 +9,59 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.android_2425_gent2.ui.AppViewModelProvider
+import com.example.android_2425_gent2.ui.navigation.AdaptiveNavigationBar
 import com.example.android_2425_gent2.ui.navigation.BottomNavigationBar
 import com.example.android_2425_gent2.ui.navigation.NavigationHost
+import com.example.android_2425_gent2.ui.screens.notification_page.NotificationViewModel
 
 @Composable
 fun MainScreen(
     windowSize: WindowWidthSizeClass,
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
+    notificationViewModel: NotificationViewModel = viewModel(factory = AppViewModelProvider.Factory),
     logout: () -> Unit = {}
 ) {
-    Scaffold(
-        modifier = modifier.testTag("MainScreen"),
-        bottomBar = {
-            BottomNavigationBar(
+    Row(modifier = modifier.testTag("MainScreen")) {
+        if (windowSize == WindowWidthSizeClass.Expanded) {
+            AdaptiveNavigationBar(
+                navController = navController,
+                windowSize = windowSize,
                 modifier = modifier,
-                navController = navController
+                notificationViewModel = notificationViewModel
             )
         }
-    ) { innerPadding ->
-        NavigationHost(
-            modifier = modifier.padding(innerPadding),
-            navController = navController,
-            windowSize = windowSize,
-            logout = logout
-        )
+
+        Scaffold(
+            modifier = if (windowSize == WindowWidthSizeClass.Expanded) {
+                modifier.weight(1f)
+            } else {
+                modifier.fillMaxWidth()
+            },
+            bottomBar = {
+                if (windowSize != WindowWidthSizeClass.Expanded) {
+                    AdaptiveNavigationBar(
+                        navController = navController,
+                        windowSize = windowSize,
+                        modifier = modifier,
+                        notificationViewModel = notificationViewModel
+                    )
+                }
+            }
+        ) { innerPadding ->
+            NavigationHost(
+                modifier = modifier.padding(innerPadding),
+                navController = navController,
+                windowSize = windowSize,
+                logout = logout
+            )
+        }
     }
 }
-
 @Preview
 @Composable
 fun MainScreenPreview() {
