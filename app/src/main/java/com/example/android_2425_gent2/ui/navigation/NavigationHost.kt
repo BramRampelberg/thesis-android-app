a71257c20bed665671b3a59f3a2dbe6ed336d367
Bram Rampelberg <bram.rampelberg@student.hogent.be>
2025-08-18 22:07:03 +0200

    fix data refetch/state loss bug when navigating

--- app/src/main/java/com/example/android_2425_gent2/ui/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/android_2425_gent2/ui/navigation/NavigationHost.kt
@@ -2,17 +2,23 @@ package com.example.android_2425_gent2.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.android_2425_gent2.ui.AppViewModelProvider
 import com.example.android_2425_gent2.ui.screens.profile_page.ProfilePage
+import com.example.android_2425_gent2.ui.screens.profile_page.ProfilePageViewModel
 import com.example.android_2425_gent2.ui.screens.reservations_page.ReservationsPage
+import com.example.android_2425_gent2.ui.screens.reservations_page.ReservationsViewModel
 
 @Composable
 fun NavigationHost(
     navController: NavHostController,
     modifier: Modifier,
     logout: () -> Unit,
+    reservationsViewModel: ReservationsViewModel = viewModel(factory = AppViewModelProvider.Factory),
+    profileViewModel: ProfilePageViewModel = viewModel(factory = AppViewModelProvider.Factory),
 ) {
     NavHost(
         navController = navController,
@@ -20,12 +26,13 @@ fun NavigationHost(
         modifier = modifier
     ) {
         composable(route = BottomNavItem.Reservations.route) {
-            ReservationsPage()
+            ReservationsPage(viewModel = reservationsViewModel)
         }
         composable(route = BottomNavItem.Profile.route) {
             ProfilePage(
                 modifier = modifier,
-                logout = logout
+                logout = logout,
+                viewModel = profileViewModel,
             )
         }
     }
--- app/src/main/java/com/example/android_2425_gent2/ui/screens/profile_page/ProfilePage.kt
+++ app/src/main/java/com/example/android_2425_gent2/ui/screens/profile_page/ProfilePage.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.android_2425_gent2.ui.AppViewModelProvider
@@ -19,18 +20,14 @@ import com.example.android_2425_gent2.ui.common.ErrorMessage
 import com.example.android_2425_gent2.ui.screens.profile_page.partials.LogOutButton
 import com.example.android_2425_gent2.ui.screens.profile_page.partials.ProfileHeader
 
+@Preview
 @Composable
 fun ProfilePage(
     modifier: Modifier = Modifier,
-    logout: () -> Unit
+    logout: () -> Unit = {},
+    viewModel: ProfilePageViewModel = viewModel(factory = AppViewModelProvider.Factory),
 ) {
-
-    val viewModel: ProfilePageViewModel = viewModel(
-        factory = AppViewModelProvider.Factory,
-    )
-
     val uiState by viewModel.uiState.collectAsState()
-
 
     Column(
         modifier = modifier
